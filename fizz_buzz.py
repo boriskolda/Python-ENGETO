a91eae33ba5a5a21bc480380bd2f3277c527a081
Boris Kolda <boris.kolda@gmail.com>
2023-07-25 16:33:38 +0200

    Fizz Buzz program

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -7,3 +7,14 @@
 # pokud bude číslo násobek 5, vypiš Buzz (místo čísla),
 # pokud bude číslo násobek 3 a zároveň násobek 5, vypiš FizzBuzz (místo čísla),
 # pokud půjde o kterékoliv jiné číslo, vypiš jej.
+
+for cislo in range(1,101):
+    if cislo % 3 == 0 and cislo % 5 == 0:
+        print("FizzBuzz")
+    elif cislo % 3 == 0:
+        print("Fizz")
+    elif cislo % 5 == 0:
+        print("Buzz")
+    else:
+        print(cislo)
+
