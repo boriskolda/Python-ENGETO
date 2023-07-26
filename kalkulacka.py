e2f56049a69a30b6c62749436a87650b7543c7ff
Boris Kolda <boris.kolda@gmail.com>
2023-07-26 15:14:40 +0200

    Kalkulacka

---
+++ kalkulacka.py
@@ -0,0 +1,41 @@
+# V této úloze vytvoř program, který si od uživatele nejprve vyžádá aritmetický operátor a následně čísla, se kterými celý zápis vypočítá.
+
+# Po každé operaci se program uživatele zeptá zda chce provést další operaci nebo chce program ukončit.
+
+# Jednotlivé kroky, které program musí obsahovat:
+
+# Vytvoř nekonečnou smyčku
+# uživatel může vybrat operátor, dostupné jsou "+", "-",
+# vstup uživatele ulož do proměnné operation,
+# pokud hodnota v proměnné operation není ("+", "-"), vypiš "Nezadali jste platný operátor, zkuste to znovu" a vrátí se na začátek běhu programu,
+# pokud je hodnota v proměnné - ("+", "-"), dovol uživateli zadat vstupy do proměnných number_1 a number_2,
+# spoj hodnoty v number_1, choice a number_2, vypočítej výsledek a vypiš např. 1 + 9 = 10,
+# zeptá se uživatele zda chce provést další operaci pokud ano program se vrátí na začátek běhu, pokud ne program se ukončí.
+
+while True:
+    print('Sčítání:    "+",')
+    print('Odčítání:   "-",')
+    print('----------------------')
+
+    while True:
+        operation = input("Vyber si operaci: ")
+        if operation not in ("+","-"):
+            print("Nezadali jste platný operátor, zkuste to znovu.")
+            continue
+        else:
+            break
+
+    number_1 = int(input("Zadej první číslo: "))
+    number_2 = int(input("Zadej druhé číslo: "))
+    
+    if operation == "+":
+        print(f"{number_1} {operation} {number_2} = {(number_1 + number_2)}")
+    elif operation == "-":
+        print(f"{number_1} {operation} {number_2} = {(number_1 - number_2)}")
+
+    while True:
+        choice = input("Chcete provést další operaci?('a' pro ano, jakákoliv jiná klávesa pro ne):")
+        if choice != 'a':
+            continue
+        else:
+            break
