158c3561eda94b05ff92595e30fd43b0f2a9b211
Boris Kolda <boris.kolda@gmail.com>
2023-07-26 22:21:28 +0200

    Minimum, maximum

---
+++ minimum_maximum.py
@@ -0,0 +1,15 @@
+# Pomocí dostupných zabudovaných funkcí zjisti ze zadaného seznamu muj_seznam minimální a maximální hodnotu.
+
+# Jednotlivé kroky, které program musí obsahovat:
+
+# Vytvoř proměnnou muj_seznam, která odkazuje na hodnotu typu list: [43, 45, 87, 21, 23],
+# uložit pomocí vhodných funkcí hodnoty do proměnných minimum a maximum,
+# pomocí jednoho výstupu vypsat obě hodnoty (viz. ukázka).
+
+muj_seznam = [43, 45, 87, 21, 23]
+
+minimum = min(muj_seznam)
+maximum = max(muj_seznam)
+
+print("Minimální hodnota je", minimum, ". Maximální hodnota je", maximum, ".")
+
